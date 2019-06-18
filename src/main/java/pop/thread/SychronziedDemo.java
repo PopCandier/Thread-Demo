01c230fc4e645b871ddb416965916311216091ed
Pop <824173124@qq.com>
2019-06-18 23:42:10 +0800

    synchronzied关键字的解释

---
+++ src/main/java/pop/thread/SychronziedDemo.java
@@ -0,0 +1,19 @@
+package pop.thread;
+
+/**
+ * @author Pop
+ * @date 2019/6/11 23:41
+ */
+public class SychronziedDemo {
+
+
+
+    public static void main(String[] args) {
+
+        synchronized (SychronziedDemo.class){
+
+        }
+
+    }
+
+}
